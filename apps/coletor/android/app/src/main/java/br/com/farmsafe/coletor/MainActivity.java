4a552fd52f18ec0fb2d73649897ebfd33ba3db6d
Carlos Eduardo <carlosalveoliveira@outlook.com>
2026-07-08 00:14:25 -0400

    feat: preparar coletor como app Android instalavel

---
+++ apps/coletor/android/app/src/main/java/br/com/farmsafe/coletor/MainActivity.java
@@ -0,0 +1,5 @@
+package br.com.farmsafe.coletor;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/coletor/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.farmsafe.coletor',
+  appName: 'Farmsafe',
+  webDir: 'dist'
+};
+
+export default config;
